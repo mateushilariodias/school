62e76e223d5652ae6a97104dac436e3d0a4d5762
mateushilariodias <mateusilariodias@gmail.com>
2023-11-16 23:58:28 -0300

    change project

--- .venv/index.py
+++ .venv/index.py
@@ -7,4 +7,36 @@ app = Flask(__name__);
 def root():
     return render_template('homepage.html')
 
+@app.route('/login-aluno')
+def loginAlunoGet():
+    return render_template('login-aluno.html')
+
+# aqui captura os valores que vem por formulario e renderiza a página 
+@app.route('/login-aluno', methods=['POST'])
+def loginAlunoPost():
+    alunoCpf = request.form['cpf']
+    alunoSenha = request.form['senha']
+
+    if alunoCpf == 'admin' and alunoSenha == '123':
+        return redirect(url_for('homepage'))
+    else:
+        return redirect(url_for('inicio', titulo="Usuário ou senha inválido"))
+    
+@app.route('/login-aluno')
+def loginAlunoGet():
+    return render_template('login-aluno.html')
+
+# aqui captura os valores que vem por formulario e renderiza a página 
+@app.route('/login-aluno', methods=['POST'])
+def loginAlunoPost():
+    alunoCpf = request.form['cpf']
+    alunoSenha = request.form['senha']
+
+    if alunoCpf == 'admin' and alunoSenha == '123':
+        return redirect(url_for('homepage'))
+    else:
+        return redirect(url_for('inicio', titulo="Usuário ou senha inválido"))
+    
+
+
 app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # school
+
+mkdir myproject
+cd myproject
+py -3 -m venv .venv
+
+.venv\Scripts\activate
+
+pip install Flask
+
+pip install mySQL.connector  
